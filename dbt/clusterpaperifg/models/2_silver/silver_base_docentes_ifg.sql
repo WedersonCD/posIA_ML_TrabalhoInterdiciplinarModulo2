9675c9d21cee1830faf0738d4bca049fb4c7b16c
wedersonCD <wedersoncabral@gmail.com>
2026-07-11 18:25:30 -0300

    finishing silver folder

--- dbt/clusterpaperifg/models/2_silver/silver_base_docentes_ifg.sql
+++ dbt/clusterpaperifg/models/2_silver/silver_base_docentes_ifg.sql
@@ -5,10 +5,10 @@ with source AS (
 ),
 src_fix_nome_da_disciplina AS (
     SELECT 
-        general_text_cleaning('nome') }     AS nome,
-        general_text_cleaning('campus') }   AS campus,
         siape                               AS id,
         siape,
+        {{ general_text_cleaning('nome') }}     AS nome,
+        {{ general_text_cleaning('campus') }}   AS campus,
         IF(EndsWith({{ general_text_cleaning('nome_da_disciplina') }},' I'),LEFT({{ general_text_cleaning('nome_da_disciplina') }},LEN({{ general_text_cleaning('nome_da_disciplina') }})-2),
         IF(EndsWith({{ general_text_cleaning('nome_da_disciplina') }},' II'),LEFT({{ general_text_cleaning('nome_da_disciplina') }},LEN({{ general_text_cleaning('nome_da_disciplina') }})-3),
         IF(EndsWith({{ general_text_cleaning('nome_da_disciplina') }},' III'),LEFT({{ general_text_cleaning('nome_da_disciplina') }},LEN({{ general_text_cleaning('nome_da_disciplina') }})-4),
@@ -21,11 +21,20 @@ src_fix_nome_da_disciplina AS (
 split_disciplina AS (
     SELECT 
         *,
-        split_part(replace(replace(disciplina,' E ','/'),':','/'),'/',1)    AS disciplina_primary,
-        split_part(replace(replace(disciplina,' E ','/'),':','/'),'/',2)    AS disciplina_secondary
+        btrim(split_part(replace(replace(replace(disciplina,' E ','/'),':','/'),',','/'),'/',1))    AS disciplina_primary,
+        btrim(split_part(replace(replace(replace(disciplina,' E ','/'),':','/'),',','/'),'/',2))    AS disciplina_secondary
     FROM
         src_fix_nome_da_disciplina
 )
 
-SELECT * FROM src_fix_nome_da_disciplina;
+SELECT 
+    id                      AS silver_base_docentes_ifg_id
+    ,siape                  AS silver_base_docentes_ifg_siape
+    ,nome                   AS silver_base_docentes_ifg_nome
+    ,campus                 AS silver_base_docentes_ifg_campus
+    ,disciplina             AS silver_base_docentes_ifg_disciplina
+    ,disciplina_primary     AS silver_base_docentes_ifg_disciplina_primary
+    ,disciplina_secondary   AS silver_base_docentes_ifg_disciplina_secondary
+FROM
+     split_disciplina;
 
